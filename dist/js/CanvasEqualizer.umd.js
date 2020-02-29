e4ad9301a1ac2f12af6f1419ae009169384c8d21
Armando Meziat <amf-commits@mezi.at>
2020-02-29 20:06:27 -0300

    Fix Rollup not including external dependencies

--- dist/js/CanvasEqualizer.umd.js
+++ dist/js/CanvasEqualizer.umd.js
@@ -1,10 +1,8 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('deep-assign')) :
-  typeof define === 'function' && define.amd ? define(['deep-assign'], factory) :
-  (global = global || self, global.CanvasEqualizer = factory(global.deepAssign));
-}(this, (function (deepAssign) { 'use strict';
-
-  deepAssign = deepAssign && deepAssign.hasOwnProperty('default') ? deepAssign['default'] : deepAssign;
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = global || self, global.CanvasEqualizer = factory());
+}(this, (function () { 'use strict';
 
   function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
@@ -110,6 +108,78 @@
   function _nonIterableRest() {
     throw new TypeError("Invalid attempt to destructure non-iterable instance");
   }
+
+  var isObj = function (x) {
+  	var type = typeof x;
+  	return x !== null && (type === 'object' || type === 'function');
+  };
+
+  var hasOwnProperty = Object.prototype.hasOwnProperty;
+  var propIsEnumerable = Object.prototype.propertyIsEnumerable;
+
+  function toObject(val) {
+  	if (val === null || val === undefined) {
+  		throw new TypeError('Sources cannot be null or undefined');
+  	}
+
+  	return Object(val);
+  }
+
+  function assignKey(to, from, key) {
+  	var val = from[key];
+
+  	if (val === undefined || val === null) {
+  		return;
+  	}
+
+  	if (hasOwnProperty.call(to, key)) {
+  		if (to[key] === undefined || to[key] === null) {
+  			throw new TypeError('Cannot convert undefined or null to object (' + key + ')');
+  		}
+  	}
+
+  	if (!hasOwnProperty.call(to, key) || !isObj(val)) {
+  		to[key] = val;
+  	} else {
+  		to[key] = assign(Object(to[key]), from[key]);
+  	}
+  }
+
+  function assign(to, from) {
+  	if (to === from) {
+  		return to;
+  	}
+
+  	from = Object(from);
+
+  	for (var key in from) {
+  		if (hasOwnProperty.call(from, key)) {
+  			assignKey(to, from, key);
+  		}
+  	}
+
+  	if (Object.getOwnPropertySymbols) {
+  		var symbols = Object.getOwnPropertySymbols(from);
+
+  		for (var i = 0; i < symbols.length; i++) {
+  			if (propIsEnumerable.call(from, symbols[i])) {
+  				assignKey(to, from, symbols[i]);
+  			}
+  		}
+  	}
+
+  	return to;
+  }
+
+  var deepAssign = function deepAssign(target) {
+  	target = toObject(target);
+
+  	for (var s = 1; s < arguments.length; s++) {
+  		assign(target, arguments[s]);
+  	}
+
+  	return target;
+  };
 
   /**
    * canvas-equalizer is distributed under the FreeBSD License
